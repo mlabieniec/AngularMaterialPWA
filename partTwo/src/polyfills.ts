514d3c31387cd41698a60f96a54c12d8b5ceebd7
Michael Labieniec <michaellabieniec@gmail.com>
2019-12-27 09:46:37 -0500

    Update for Angular 8. Fixes #5

--- partTwo/src/polyfills.ts
+++ partTwo/src/polyfills.ts
@@ -1,4 +1,7 @@
 (window as any).global = window;
+(window as any).process = {
+    env: { DEBUG: undefined },
+};
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
