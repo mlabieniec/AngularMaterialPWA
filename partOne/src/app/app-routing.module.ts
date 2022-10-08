1c409c71d24e78e8e041ac4e9daec70c46a92889
Michael Labieniec <michaellabieniec@gmail.com>
2022-10-08 07:35:14 -0400

    upgrade angular

--- partOne/src/app/app-routing.module.ts
+++ partOne/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'auth', component: HomeComponent }
 ];
 
 @NgModule({
